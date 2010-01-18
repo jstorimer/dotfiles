26ca9f054b87a1fd3632ab8c56000b0bc901104e
Jesse Storimer <jesse@jadedpixel.com>
2010-01-18 10:56:16 -0500

    Fish shell stuff. Still working on cleaning it up. Mostly from @Ben_h

---
+++ config/fish/config.fish
@@ -0,0 +1,42 @@
+if status --is-login
+	for p in /usr/bin /usr/local/bin /opt/local/bin /usr/local/mysql/bin /opt/local/lib/postgresql83/bin ~/bin ~/.config/fish/bin 
+		if test -d $p
+			set PATH $p $PATH
+		end
+	end
+end
+
+# Textmate defaults to /usr/bin/mate, but let's check for /usr/local/bin/mate
+if test -f "/usr/local/bin/mate"
+	set -x EDITOR "/usr/local/bin/mate -w"
+else
+	set -x EDITOR "/usr/bin/mate -w"
+end
+
+set fish_greeting ""
+set -x CLICOLOR 1
+
+function parse_git_branch
+	sh -c 'git branch --no-color 2> /dev/null' | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+end
+
+function parse_svn_revision
+	sh -c 'svn info 2> /dev/null' | sed -n '/^Revision/p' | sed -e 's/^Revision: \(.*\)/\1/'
+end
+
+set BROWSER open
+
+bind \cr "rake"
+
+function ss -d "Run the script/server"
+	script/server
+end
+
+function sc -d "Run the Rails console"
+	script/console
+end
+
+set -x JAVA_HOME "/usr/"
+
+
+
---
+++ config/fish/functions/e.fish
@@ -0,0 +1,3 @@
+function e
+	cd ~/envato/marketplace $argv; 
+end
